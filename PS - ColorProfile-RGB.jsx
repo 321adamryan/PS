cbc307f2ece14f9d41ebdde7fc47a007998ac11a
Adam Ryan <51381387+lazryn@users.noreply.github.com>
2019-06-10 08:19:07 -0400

    Add files via upload

---
+++ PS - ColorProfile-RGB.jsx
@@ -0,0 +1,28 @@
+﻿#target photoshop
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//
+//       filename: PS - ColorProfile-RGB.jsx
+//          coder: AdamRyan
+//        program: Adobe Photoshop
+//    description: Sets the documents color profile to RGB
+//      extention: JSX
+//       licensce: OpenSource
+//        website: adamryan.info
+//
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+// in case we double clicked the file
+app.bringToFront();
+
+main();
+
+function main() {
+	if ( app.documents.length <= 0 ) {
+		alert("You must have a document open to change color profile");
+		return;
+	}
+	
+	// Convert the Profile to sRGB
+	app.activeDocument.convertProfile("sRGB IEC61966-2.1", Intent.RELATIVECOLORIMETRIC);
+    }
